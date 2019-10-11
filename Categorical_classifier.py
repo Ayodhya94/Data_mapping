283a88de46506a0c137388c8c09450857c756009
Ayodhya <140597V@uom,lk>
2019-10-11 11:51:59 +0530

    added new method

--- Categorical_classifier.py
+++ Categorical_classifier.py
@@ -41,24 +41,19 @@ def train_categories_stat(batch_x, batch_y, num_class):
     return
 
 
-def train_categories_char2vec(x, y):
+def train_categories_char2vec(x, y, num_class, tag_list):
     x_train = np.asarray(x)
     y_train = np.asarray(y)
 
-    max_review_length = 40  # Length of sequence
-
-    # x_train_padded = []
-
-    # for entry in x_train:
-    #     if len(entry) < max_review_length:
-    #         for num in range(max_review_length-len(entry)):
-    #             entry = np.append(entry, [[0*i for i in range(50)]], axis=0)
-    #     x_train_padded.append(entry)
+    # # Test set
+    # x_test = np.asarray(x[180:200] + x[380:400] + x[580:600] + x[780:800])
+    # y_test = np.asarray(y[180:200] + y[380:400] + y[580:600] + y[780:800])
+    # tags = tag_list[180:200] + tag_list[380:400] + tag_list[580:600] + tag_list[780:800]
 
     # create the model
     model = Sequential()
-    model.add(LSTM(10))
-    model.add(Dense(2, activation='softmax'))
+    model.add(LSTM(50))
+    model.add(Dense(num_class, activation='softmax'))
     sgd = optimizers.SGD(lr=0.01, momentum=0.9)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
@@ -68,15 +63,52 @@ def train_categories_char2vec(x, y):
 
     # Train model
     print("Start training...")
-    model.fit(np.asarray(x_train), y_train, epochs=20, batch_size=32, shuffle=True,)
+    model.fit(np.asarray(x_train), y_train, epochs=100, batch_size=128, shuffle=True,)
     print("Trained")
 
     # # Final evaluation of the model
-    # scores = model.evaluate(np.asarray(x_test_padded), y_test, verbose=0)
+    # scores = model.evaluate(np.asarray(x_test), y_test, verbose=0)
     # print("Accuracy: %.2f%%" % (scores[1] * 100))
 
     # Save model
     model.save("Categorical_classifier.h5")
+    print("Model saved to Categorical_classifier.h5")
+    return
+
+
+def train_categories_embedding(x, y, num_class, tag_list):
+    x_train = np.asarray(x)
+    y_train = np.asarray(y)
+
+    # # Test set
+    # x_test = np.asarray(x[180:200] + x[380:400] + x[580:600] + x[780:800])
+    # y_test = np.asarray(y[180:200] + y[380:400] + y[580:600] + y[780:800])
+    # tags = tag_list[180:200] + tag_list[380:400] + tag_list[580:600] + tag_list[780:800]
+
+    # create the model
+    model = Sequential()
+    model.add(Embedding(93, 50, input_length=None))
+    model.add(LSTM(10))
+    model.add(Dense(num_class, activation='softmax'))
+    sgd = optimizers.SGD(lr=0.01, momentum=0.9)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    # sparse_categorical_crossentropy
+
+    # # Print model summary
+    # model.build((None, 40, 50))
+    # print(model.summary())
+
+    # Train model
+    print("Start training...")
+    model.fit(np.asarray(x_train), y_train, epochs=100, batch_size=32, shuffle=True,)
+    print("Trained")
+
+    # # Final evaluation of the model
+    # scores = model.evaluate(np.asarray(x_test), y_test, verbose=0)
+    # print("Accuracy: %.2f%%" % (scores[1] * 100))
+
+    # Save model
+    model.save("Categorical_classifier_embedd.h5")
     print("Model saved to Categorical_classifier.h5")
     return
 
@@ -99,7 +131,6 @@ def get_features_stat(word):
 
 
 def get_features_char2vec(word, max_review_length, c2v_model):
-    # c2v_model = chars2vec.load_model('eng_50')
     chars = list(word)
     features = []
 
@@ -114,9 +145,31 @@ def get_features_char2vec(word, max_review_length, c2v_model):
         features.append(norm_char2vec_list)
 
     # Zero padding
+    features_2 = []
     if len(features) < max_review_length:
         for num in range(max_review_length - len(features)):
-            features.append([0 * i for i in range(50)])
+            features_2.append([0 * i for i in range(50)])
+    features = features_2 + features
+    return features
+
+
+def get_features_embedding(word, max_review_length, char_dic):
+    chars = list(word)
+    features = []
+
+    for char in chars:
+        try:
+            char2vec = char_dic[char]
+            features.append(char2vec)
+        except KeyError:
+            continue
+
+    # Zero padding
+    features_2 = []
+    if len(features) < max_review_length:
+        for num in range(max_review_length - len(features)):
+            features_2.append(0)
+    features = features_2 + features
     return features
 
 
@@ -127,6 +180,26 @@ def predict_char2vec(word, model, c2v_model, max_review_length):
     features = np.asarray(feature)
     prediction = model.predict(features)
     return prediction
+
+
+def predict_embedding(word, embedding_model, char_dic, max_review_length):
+    feature = []
+    feature.append(get_features_embedding(word, max_review_length, char_dic))
+
+    features = np.asarray(feature)
+    prediction = embedding_model.predict(features)
+    return prediction
+
+
+def create_dic():
+    alphanum = list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=`~[]{}|;:,.<>/? ')
+    char_dic = {}
+    i = 1
+    for char in alphanum:
+        char_dic[char] = i
+        i += 1
+    print(char_dic)
+    return char_dic
 
 
 def main():
@@ -152,42 +225,86 @@ def main():
     x_batch = []
     y_batch = []
 
-    max_review_length = 40
-
+    max_review_length = 50
+    c2v_model = chars2vec.load_model('eng_50')
+    num_class = 5
+    char_dic = create_dic()
+    """
+    # Get features
     f = open("Datasets/categ.txt", "r")
     fl = f.readlines()
-
-    # Get features
-    c2v_model = chars2vec.load_model('eng_50')
     i = 0
+    tag_list = []
     for ln in fl:
         i = i + 1
         # print(i)
+        tag_list.append(ln)
         x_batch.append(get_features_char2vec(ln, max_review_length, c2v_model))
+    print(len(x_batch))
 
     # Output data set
     f = open("Datasets/outs.txt", "r")
     fl = f.readlines()
     for ln in fl:
-        if ln == "0\n":
-            y_batch.append([1, 0])
-            # y_batch.append([0])
-        else:
-            y_batch.append([0, 1])
-            # y_batch.append([1])
+        out = [0*i for i in range(num_class)]
+        out[int(ln)] = 1
+        y_batch.append(out)
+    print(len(y_batch))
 
     # Train model
-    train_categories_char2vec(x_batch, y_batch)
-
+    train_categories_char2vec(x_batch, y_batch, num_class, tag_list)
+    """
     # Predict
     model = load_model("Categorical_classifier.h5")
-    test = "ayodhyakinkini@gmaIL.COM"
+    
+    test = "South Georgia And The South Sandwich Islands"
     prediction = predict_char2vec(test, model, c2v_model, max_review_length).tolist()[0]
     print(prediction)
     print(max(prediction))
-    classes = ["Email", "Phone"]
+    classes = ["Email", "Phone", "Name", "Country", "street"]
     print("%s is a %s" % (test, classes[prediction.index(max(prediction))]))
 
+    print(np.dot(c2v_model.vectorize_words(["paper"])[0], c2v_model.vectorize_words(["pencil"])[0]))
+    print(c2v_model.vectorize_words(["2"])[0] == c2v_model.vectorize_words(["1"])[0])
+    """
+
+    # ############## Embedding
+    # Get features
+    f = open("Datasets/categ.txt", "r")
+    fl = f.readlines()
+    i = 0
+    tag_list = []
+    for ln in fl:
+        i = i + 1
+        # print(i)
+        tag_list.append(ln)
+        x_batch.append(get_features_embedding(ln[:-1], max_review_length, char_dic))
+    print(len(x_batch))
+
+    # Output data set
+    f = open("Datasets/outs.txt", "r")
+    fl = f.readlines()
+    for ln in fl:
+        out = [0 * i for i in range(num_class)]
+        out[int(ln)] = 1
+        y_batch.append(out)
+    print(len(y_batch))
+
+    # Train model
+    train_categories_embedding(x_batch, y_batch, num_class, tag_list)
+
+    # Predict
+    embedding_model = load_model("Categorical_classifier_embedd.h5")
+
+    test = "Ayodhya Somiruwan"
+    prediction = predict_embedding(test, embedding_model, char_dic, max_review_length).tolist()[0]
+    print(prediction)
+    print(max(prediction))
+    classes = ["Email", "Phone", "Name", "Country", "street"]
+    print("%s is a %s" % (test, classes[prediction.index(max(prediction))]))
+
+    # ################################
+    """
     return
 
 
