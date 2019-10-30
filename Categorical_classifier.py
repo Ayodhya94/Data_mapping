24c0efd13b9c63bd8998fa76f94df173f65e881f
Ayodhya <140597V@uom,lk>
2019-10-30 10:17:14 +0530

    Update Categorical_classifier.py

--- Categorical_classifier.py
+++ Categorical_classifier.py
@@ -26,17 +26,6 @@ def train_categories_stat(batch_x, batch_y, num_class):
     steps = 1000  # The number of training iterations
 
     model = xgb.train(param, d_train, steps)
-    # print("Feature Importance:")
-    # print("Weight:")
-    # print(model.get_score(importance_type='weight'))
-    # print("Gain:")
-    # print(model.get_score(importance_type='gain'))
-    # print("Cover:")
-    # print(model.get_score(importance_type='cover'))
-    # print("Total gain:")
-    # print(model.get_score(importance_type='total_gain'))
-    # print("Total cover:")
-    # print(model.get_score(importance_type='total_cover'))
     joblib.dump(model, "Categorical_classifier_model")
     return
 
@@ -45,34 +34,21 @@ def train_categories_char2vec(x, y, num_class, tag_list):
     x_train = np.asarray(x)
     y_train = np.asarray(y)
 
-    # # Test set
-    # x_test = np.asarray(x[180:200] + x[380:400] + x[580:600] + x[780:800])
-    # y_test = np.asarray(y[180:200] + y[380:400] + y[580:600] + y[780:800])
-    # tags = tag_list[180:200] + tag_list[380:400] + tag_list[580:600] + tag_list[780:800]
-
-    # create the model
+    '''create the model'''
     model = Sequential()
     model.add(LSTM(50))
     model.add(Dense(num_class, activation='softmax'))
     sgd = optimizers.SGD(lr=0.01, momentum=0.9)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-    # # Print model summary
-    # model.build((None, 40, 50))
-    # print(model.summary())
-
-    # Train model
+    '''Train model'''
     print("Start training...")
     model.fit(np.asarray(x_train), y_train, epochs=150, batch_size=128, shuffle=True,)
     print("Trained")
 
-    # # Final evaluation of the model
-    # scores = model.evaluate(np.asarray(x_test), y_test, verbose=0)
-    # print("Accuracy: %.2f%%" % (scores[1] * 100))
-
-    # Save model
-    model.save("Categorical_classifier.h5")
-    print("Model saved to Categorical_classifier.h5")
+    '''Save model'''
+    model.save("Categorical_classifier_models/Categorical_classifier_test.h5")
+    print("Model saved to Categorical_classifier_test.h5")
     return
 
 
@@ -80,36 +56,22 @@ def train_categories_embedding(x, y, num_class, tag_list):
     x_train = np.asarray(x)
     y_train = np.asarray(y)
 
-    # # Test set
-    # x_test = np.asarray(x[180:200] + x[380:400] + x[580:600] + x[780:800])
-    # y_test = np.asarray(y[180:200] + y[380:400] + y[580:600] + y[780:800])
-    # tags = tag_list[180:200] + tag_list[380:400] + tag_list[580:600] + tag_list[780:800]
-
-    # create the model
+    '''create the model'''
     model = Sequential()
     model.add(Embedding(93, 50, input_length=None))
     model.add(LSTM(10))
     model.add(Dense(num_class, activation='softmax'))
     sgd = optimizers.SGD(lr=0.01, momentum=0.9)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-    # sparse_categorical_crossentropy
 
-    # # Print model summary
-    # model.build((None, 40, 50))
-    # print(model.summary())
-
-    # Train model
+    '''Train model'''
     print("Start training...")
     model.fit(np.asarray(x_train), y_train, epochs=150, batch_size=32, shuffle=True,)
     print("Trained")
 
-    # # Final evaluation of the model
-    # scores = model.evaluate(np.asarray(x_test), y_test, verbose=0)
-    # print("Accuracy: %.2f%%" % (scores[1] * 100))
-
-    # Save model
-    model.save("Categorical_classifier_embedd.h5")
-    print("Model saved to Categorical_classifier.h5")
+    '''Save model'''
+    model.save("Categorical_classifier_models/Categorical_classifier_embedd_test.h5")
+    print("Model saved to Categorical_classifier_test.h5")
     return
 
 
@@ -211,43 +173,15 @@ def test_model(embedding_model, char_dic, max_review_length):
         i += 1
         print(i)
         prediction = predict_embedding(line, embedding_model, char_dic, max_review_length).tolist()[0]
-        # print(prediction)
-        # print(max(prediction))
         print("%s is a %s" % (line, classes[prediction.index(max(prediction))]))
     return
 
 
-def main():
-    """ Classes
-    0 - Name
-    1 - Address
-    2 - Phone
-    3 - Email
-    4 - Country
-    5 - ID
-    6 - Code
-
-    Name
-    City
-    Country
-    Phone
-    Email
-
-
-    """
-
-    # Training data preparation
+def train_model(f_train_in, f_train_out, max_review_length, c2v_model, num_class):
     x_batch = []
     y_batch = []
 
-    max_review_length = 50
-    c2v_model = chars2vec.load_model('eng_50')
-    num_class = 9
-    char_dic = create_dic()
-    """
-    # Get features
-    f = open("Datasets/categ.txt", "r")
-    fl = f.readlines()
+    fl = f_train_in.readlines()
     i = 0
     tag_list = []
     for ln in fl:
@@ -258,8 +192,7 @@ def main():
     print(len(x_batch))
 
     # Output data set
-    f = open("Datasets/outs.txt", "r")
-    fl = f.readlines()
+    fl = f_train_out.readlines()
     for ln in fl:
         out = [0*i for i in range(num_class)]
         out[int(ln)] = 1
@@ -268,62 +201,23 @@ def main():
 
     # Train model
     train_categories_char2vec(x_batch, y_batch, num_class, tag_list)
-    """
-    # Predict
-    model = load_model("Categorical_classifier.h5")
-    
-    test = "December"
-    prediction = predict_char2vec(test, model, c2v_model, max_review_length).tolist()[0]
-    print(prediction)
-    print(max(prediction))
-    classes = ["Email", "Phone", "Name", "Country", "street", "Time", "Month", "Day"]
-    print("%s is a %s" % (test, classes[prediction.index(max(prediction))]))
+    return
 
-    # print(np.dot(c2v_model.vectorize_words(["paper"])[0], c2v_model.vectorize_words(["pencil"])[0]))
-    # print(c2v_model.vectorize_words(["2"])[0] == c2v_model.vectorize_words(["1"])[0])
-    """
 
-    # ############## Embedding
-    # Get features
-    f = open("Datasets/categ.txt", "r")
-    fl = f.readlines()
-    i = 0
-    tag_list = []
-    for ln in fl:
-        i = i + 1
-        # print(i)
-        tag_list.append(ln)
-        x_batch.append(get_features_embedding(ln[:-1], max_review_length, char_dic))
-    print(len(x_batch))
+def main():
+    max_review_length = 50
+    c2v_model = chars2vec.load_model('eng_50')
+    num_class = 9
+    char_dic = create_dic()
+    f_train_in = open("Datasets/categ.txt", "r")
+    f_train_out = open("Datasets/outs.txt", "r")
 
-    # Output data set
-    f = open("Datasets/outs.txt", "r")
-    fl = f.readlines()
-    for ln in fl:
-        out = [0 * i for i in range(num_class)]
-        out[int(ln)] = 1
-        y_batch.append(out)
-    print(len(y_batch))
+    ''' Training model'''
+    train_model(f_train_in, f_train_out, max_review_length, c2v_model, num_class)
 
-    # Train model
-    train_categories_embedding(x_batch, y_batch, num_class, tag_list)
-    """
-
-    # Predict
-    embedding_model = load_model("Categorical_classifier_embedd.h5")
-
-    test = "Male"
-    prediction = predict_embedding(test, embedding_model, char_dic, max_review_length).tolist()[0]
-    print(prediction)
-    print(max(prediction))
-    classes = ["Email", "Phone", "Name", "Country", "street", "Time", "Month", "Day", "Gender", "Date", "Currency",
-               "Postcode"]
-    print("%s is a %s" % (test, classes[prediction.index(max(prediction))]))
-
-    # ################################
-
+    '''Predict'''
+    embedding_model = load_model("Categorical_classifier_models/Categorical_classifier_embedd.h5")
     test_model(embedding_model, char_dic, max_review_length)
-
     return
 
 
